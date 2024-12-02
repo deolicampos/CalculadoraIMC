34db92bb0d38637d271e6ee43016cd9c7e52233b
Eduardo Campos <deoli.cmps@gmail.com>
2024-12-02 01:59:50 -0400

    Criado a Intent e a ResultActivity para pegar o cálculo do IMC e enviar para a segunda tela, além da lógica para classificar a pessoa de acordo com o IMC calculado.

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.comunidadedevspace.imc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -26,7 +27,12 @@ class MainActivity : AppCompatActivity() {
                 val peso = pesoStr.toFloat()
                 val altura = alturaStr.toFloat()
                 val resultado = peso/(altura*altura)
-                println("O IMC é: "+ resultado)
+
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra(KEY_RESULT_IMC, resultado)
+                startActivity(intent)
+
+                println("O IMC é: " + resultado)
 
             }else {
                 Snackbar
